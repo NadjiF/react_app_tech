3931d5e5c8cc7c8b9ac11037584f1dde10e09f6e
NadjiF <fali.nadji@hotmail.fr>
2023-04-20 16:16:33 +0200

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>My progession</h1>
+      <div>Welcome to this site devoted to your programmming</div>
+    </div>
+  );
+}
